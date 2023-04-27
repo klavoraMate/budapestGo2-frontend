42319c8acb4dc4f04e53d09437ea24dc4cb4a6b7
klavoraMate <klavoramate98@gmail.com>
2023-04-27 10:58:06 +0200

    feat: email now displayed in home page after successful login

--- budapest-go-2-frontend/src/components/Home.js
+++ budapest-go-2-frontend/src/components/Home.js
@@ -1,13 +1,28 @@
 import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Home = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const [emailCookie, setEmailCookie] = useState(null);
+    const [privilegeCookie, setPrivilegeCookie] = useState(null);
+
     const navigateTo = (urlEnd) => {
         navigate(urlEnd);
     };
 
+    useEffect(() => {
+        const cookies = document.cookie.split("; ");
+        const emailCookie = cookies.find((cookie) => cookie.startsWith("email="));
+        const privilegeCookie = cookies.find((cookie) => cookie.startsWith("privilege="));
+        if (emailCookie && privilegeCookie) {
+            setEmailCookie(emailCookie.split("=")[1]);
+            setPrivilegeCookie(privilegeCookie.split("=")[1]);
+        }
+    }, []);
+
     return (
         <>
+            {emailCookie && <p>{emailCookie}</p>}
             <h1>Budapest Go 2</h1>
             <button className="login" onClick={() => navigateTo('./login')}>Login</button>
         </>
