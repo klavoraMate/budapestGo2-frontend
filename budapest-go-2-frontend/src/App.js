819daf12e88e962ecbdc4626f36f64292f5c9f26
Norbert Remenyi <remenyinorbert@gmail.com>
2023-04-27 11:53:57 +0200

    add registerpage

--- budapest-go-2-frontend/src/App.js
+++ budapest-go-2-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LoginPage from './components/LoginPage';
+import RegisterPage from './components/client/RegisterPage';
 import Home from './components/Home';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/register' element={<RegisterPage/>} />
     </Routes>
   </Router >);
 }
