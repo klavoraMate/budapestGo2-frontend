364406042b9e49226507e48e78db779ee8e1b190
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-04-27 11:41:59 +0200

    feat: Add html structure to RouteCreate

--- budapest-go-2-frontend/src/components/workspace/route/RouteCreate.js
+++ budapest-go-2-frontend/src/components/workspace/route/RouteCreate.js
@@ -2,7 +2,21 @@ import React from 'react'
 
 function RouteCreate() {
   return (
-    <div>routeCreate</div>
+    <div>
+      <h2>Create new transportation route</h2>
+      <div className='routeDetail'>
+        <p>Set name of new route:</p>
+        <input type='text'></input>
+      </div>
+      <div className='stopPanel'>
+        <p>Assign selected stop to route</p>
+        <select>
+          <option>A</option>
+        </select>
+        <button>Assign stop</button>
+      </div>
+      <button>Create route</button>
+    </div>
   )
 }
 
