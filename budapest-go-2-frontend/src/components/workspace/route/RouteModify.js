4cf0cef53e013e7bd4813a8594104e1482ff19f2
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-10 19:22:41 +0200

    feat: Add ListViews to the page. Split into two side the content

--- budapest-go-2-frontend/src/components/workspace/route/RouteModify.js
+++ budapest-go-2-frontend/src/components/workspace/route/RouteModify.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useEffect, useState, useRef } from 'react';
 import useMultiFetch from '../../api/useMultiFetch';
+import ListView from '../../elements/ListView';
+import './routeModify.css';
 
 function RouteModify() {
   const [listOfStops, setListOfStops] = useState();
@@ -9,10 +11,12 @@ function RouteModify() {
   const routeDropdown = useRef();
   const routeNewName = useRef();
   const stopDropdown = useRef();
+  const listViewAvailableStop = useRef();
+  const listViewAssignedStop = useRef();
   const { data } = useMultiFetch();
 
   /*
-    Updateing: Removing and assigning stops to the route in schedule.
+    TODO: Implement removeStopToList. Rework route update to not make a new route. Updateing: Removing and assigning stops to the route in schedule.
   */
 
   useEffect(() => {
@@ -24,13 +28,17 @@ function RouteModify() {
   const addStopToList = () => {
     const nameOfRoute = routeDropdown.current.value;
     const nameofStop = stopDropdown.current.value;
-    if (!listOfAssignedStop.includes(nameofStop)) {
-      listOfAssignedStop.push(nameofStop)
+    const stopObject = {name: nameofStop};
+    if (!listOfAssignedStop.includes(stopObject)) {
+      listOfAssignedStop.push(stopObject)
       console.log(nameofStop + ' assigned to ' + nameOfRoute + ' current length of route is: ' + listOfAssignedStop.length);
     }
     else {
       console.log("Selected stop is already assigned to this route!");
     }
+  }
+  const removeStopFromList = () => {
+
   }
 
   const deleteRouteFromSchedule = async () => {
@@ -58,22 +66,41 @@ function RouteModify() {
   return (
     <div className='pageContent'>
       <h2>Modify transportation route</h2>
-      <div className='routeDetail'>
-        <p>Select existing route:</p>
-        <select ref={routeDropdown}>
-          {listOfRoutes && listOfRoutes.map((route) => <option key={route.name}>{route.name}</option>)}
-        </select>
-        <p>Rename selected route</p>
-        <input ref={routeNewName}/>
+      <div className='pagePanel'>
+        <div className='pageElement'>
+          <div className='routeDetail'>
+            <p>Select existing route:</p>
+            <select ref={routeDropdown} onChange={() => routeNewName.current.value = routeDropdown.current.value}>
+              {listOfRoutes && listOfRoutes.map((route) => <option key={route.name}>{route.name}</option>)}
+            </select>
+            <p>Rename selected route</p>
+            <input ref={routeNewName} defaultValue={listOfRoutes && listOfRoutes[0].name}/>
+          </div>
+          <div className='stopPanel'>
+            <p>Assign selected stop to route</p>
+            <select ref={stopDropdown}>
+              {listOfStops && listOfStops.map((stop) => <option key={stop.name}>{stop.name}</option>)}
+            </select>
+            <button onClick={() => addStopToList()}>Assign stop</button>
+          </div>
+          <button onClick={() => updateRoute()}>Update route</button>
+        </div>
+
+        <div className='pageElement'>
+          <div className='listPanel'>
+            <div className='element'>
+              <p>Available stops</p>
+              <ListView listElements={listOfStops} ref={listViewAvailableStop}/>
+              <button onClick={() => addStopToList()}>{'Assign (>)'}</button>
+            </div>
+            <div className='element'>
+              <p>Assigned stops</p>
+              <ListView listElements={listOfAssignedStop} ref={listViewAssignedStop}/>
+              <button onClick={() => removeStopFromList()}>{'Remove (<)'}</button>
+            </div>
+          </div>
+        </div>
       </div>
-      <div className='stopPanel'>
-        <p>Assign selected stop to route</p>
-        <select ref={stopDropdown}>
-          {listOfStops && listOfStops.map((stop) => <option key={stop.name}>{stop.name}</option>)}
-        </select>
-        <button onClick={() => addStopToList()}>Assign stop</button>
-      </div>
-      <button onClick={() => updateRoute()}>Update route</button>
     </div>
   )
 }
