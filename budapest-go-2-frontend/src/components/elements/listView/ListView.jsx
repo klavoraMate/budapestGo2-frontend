35035e5b74952607a7112798c8fa7f8815946112
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-11 02:23:55 +0200

    chore: ListView element dependencies moved into their own package

--- budapest-go-2-frontend/src/components/elements/listView/ListView.jsx
+++ budapest-go-2-frontend/src/components/elements/listView/ListView.jsx
@@ -1,37 +1,34 @@
 import React, { useEffect, useState, forwardRef } from "react";
 import "./listView.css";
-/*
-    TODO: Experiencing with changing variable from ref.current.value to value.
-*/
 
 const ListView = forwardRef(({ listElements }, ref) => {
     const [selectedOption, setSelectedOption] = useState(null);
+    const [selectedObject, setSelectedObject] = useState(null);
     const [prevSelectedOption, setPrevSelectedOption] = useState(null);
 
     useEffect(() => {
         prevSelectedOption && prevSelectedOption.classList.remove("selectedOption");
         selectedOption && selectedOption.classList.add("selectedOption");
-    }, [selectedOption]);
+    }, [selectedOption, prevSelectedOption]);
 
-    const handleOnOptionClick = (event) => {
+    const handleOnOptionClick = (element, event) => {
         const target = event.target;
         setPrevSelectedOption(selectedOption);
         setSelectedOption(target);
+        setSelectedObject(listElements.filter((stop) => stop.id === element.id)[0]);
     };
 
     useEffect(() => {
         if (ref && ref.current) {
             ref.current.value = (selectedOption&&selectedOption.innerText)??null;
-            ref.current.selected = (selectedOption&&selectedOption)??null;
-
+            ref.current.selected = selectedObject??{};
         }
     }, [ref, selectedOption]);
-
     return (
         <div className="listBody" ref={ref}>
             <div className="column">
                 {listElements && listElements.map((element) => (
-                        <div key={element.name} className="option" onClick={handleOnOptionClick}>
+                        <div key={element.id} className="option" onClick={(event) => handleOnOptionClick(element, event)}>
                             {element.name}
                         </div>
                     ))}
