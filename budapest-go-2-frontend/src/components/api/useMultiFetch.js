9785ef486930881d62d95951bf0bf82cc9887adb
Kieferer <balazsborsi0@gmail.com>
2023-05-24 18:11:55 +0200

    fix: useMultiFetch able to return non-JSON responses

--- budapest-go-2-frontend/src/components/api/useMultiFetch.js
+++ budapest-go-2-frontend/src/components/api/useMultiFetch.js
@@ -1,28 +1,37 @@
 import { token } from "../token/TokenDecoder";
+
 const useMultiFetch = () => {
-    const data = async (url, method, answerObject) => {
-      try {
-        const response = await fetch((!(url[0] === "/") ? "/" : "") + url, {
-          method: (method ?? "GET"),
-          body: JSON.stringify(answerObject),
-          headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${token()}`
-          }
-        });
-        if (!response.ok) {
-          console.log(answerObject);
-          throw new Error(`Failed to ${method ?? "GET"} to table: ${url}`);
-        }
-        try {
-          return await response.json();
-        } catch (error){
-          return error;
-        }
-      } catch (error) {
-        console.error(error);
+  const data = async (url, method, answerObject) => {
+    try {
+      const tokenValue = token();
+      const response = await fetch((url[0] === "/" ? "" : "/") + url, {
+        method: method ?? "GET",
+        body: answerObject ? JSON.stringify(answerObject) : undefined,
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${tokenValue}`,
+        },
+      });
+
+      if (!response.ok) {
+        const errorMessage = `Failed to ${method ?? "GET"} to table: ${url}`;
+        console.error(errorMessage);
+        throw new Error(errorMessage);
       }
-    };
-    return { data };
+
+      const contentType = response.headers.get("content-type");
+      if (contentType && contentType.includes("application/json")) {
+        return await response.json();
+      } else {
+        return await response.text();
+      }
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
   };
-  export default useMultiFetch;
+
+  return { data };
+};
+
+export default useMultiFetch;
