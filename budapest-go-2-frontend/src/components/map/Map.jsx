845b5368b0871b5d99c7fc2459426b6079899fde
Kieferer <76512983+Kieferer@users.noreply.github.com>
2023-05-25 14:12:55 +0200

    feat: Add selectRoute and isUpdate, imports refactored

--- budapest-go-2-frontend/src/components/map/Map.jsx
+++ budapest-go-2-frontend/src/components/map/Map.jsx
@@ -1,35 +1,34 @@
-import {useEffect, useState} from 'react'
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import useMultiFetch from "../api/useMultiFetch";
+import {useEffect, useState} from 'react'
+import L from "leaflet";
 import './leaflet.css';
 import './map.css'
-import useMultiFetch from "../api/useMultiFetch";
-import L from "leaflet";
 const getIconPath = (vehicleCategory) => {
   const iconName = "-marker-icon.png";
   const publicPath = process.env.PUBLIC_URL + "/map/";
-  return publicPath + vehicleCategory.toLowerCase() + iconName;
+  return publicPath + (vehicleCategory.toLowerCase() ?? "default") + iconName;
 }
-const getIcon = (vehicleCategory) => {
-  return L.icon({
-    iconUrl: getIconPath(vehicleCategory),
-    iconSize: [25, 41]
+const getIcon = (vehicle) => {
+      return L.icon({
+          iconUrl: getIconPath(vehicle.category),
+          iconSize: [25, 41]
   })
 }
 const Map = () => {
-    const { data } = useMultiFetch();
-    const [listOfStops, setListOfStops] = useState([]);
-    const [listOfStopsOfRoute, setListOfStopsOfRoute] = useState([]);
-    const [listOfRoutes, setListOfRoutes] = useState([]);
-    const locationOfBudapest = [47.486208, 19.108459];
-    useEffect(() => {
-        const stopURL = '/stop/all';
-        const routeURL = '/route/all';
-        (async () => setListOfStops(await data(stopURL), setListOfRoutes(await data(routeURL))))();
-    }, [data])
+  const { data } = useMultiFetch();
+  const [listOfStopsOfRoute, setListOfStopsOfRoute] = useState([]);
+  const [listOfRoutes, setListOfRoutes] = useState([]);
+  const [selectedRoute, setSelectedRoute] = useState(null);
+  const [isUpdated, setUpdate] = useState(true);
+  const locationOfBudapest = [47.486208, 19.108459];
 
   useEffect(() => {
-    console.log("update")
-  }, [listOfStopsOfRoute])
+    const stopURL = '/stop/all';
+    const routeURL = '/route/all';
+    (async () => setListOfRoutes(await data(routeURL)))();
+    }, [data]
+  );
 
   const search = (event) => {
     const searchText = event.target.value;
@@ -57,18 +56,14 @@ const Map = () => {
     results.sort((a, b) => b[0] - a[0]);
 
     const route = results[0][1];
+    setSelectedRoute(route);
     const scheduleURL = '/schedule/stops-connected-to-route-id/' + route.id;
     event.target.value = route.name;
     (async () => setListOfStopsOfRoute(await data(scheduleURL)))();
-
-    /*for (let allStop = 0; allStop < listOfStops.length; allStop++){
-      for (let selectedStop = 0; selectedStop < listOfStopsOfRoute.length; selectedStop++){
-        console.log([listOfStops[allStop].id, listOfStopsOfRoute[selectedStop].id])
-      }
-    }*/
+    setUpdate(true);
   };
 
-
+  if (isUpdated)
   return (
       <div className={"mapContent"}>
         <input className={"searchBar"} type={"text"} placeholder={"Search routes, lines, stops or spots"} onKeyDown={(event) => (event.code === 'Enter') && search(event)}/>
@@ -77,7 +72,7 @@ const Map = () => {
             attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-            {listOfStopsOfRoute&&listOfStopsOfRoute.map(stop => <Marker key={stop.id} icon={getIcon(stop.category)} position={[stop.location.latitude, stop.location.longitude]}>
+            {listOfStopsOfRoute&&listOfStopsOfRoute.map(stop => <Marker key={stop.id} icon={getIcon(selectedRoute)} position={[stop.location.latitude, stop.location.longitude]}>
                 <Popup>
                     {stop.name}
                 </Popup>
