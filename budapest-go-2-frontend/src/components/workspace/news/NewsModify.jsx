4892bbe64bff54316ea6b6e7c76f988a0824647c
Norbert Remenyi <remenyinorbert@gmail.com>
2023-05-25 20:30:02 +0200

    feat: news update with new image

--- budapest-go-2-frontend/src/components/workspace/news/NewsModify.jsx
+++ budapest-go-2-frontend/src/components/workspace/news/NewsModify.jsx
@@ -10,17 +10,14 @@ export const NewsModify = () => {
     const [listOfNews, setListOfNews] = useState([]);
     const [isLoaded, setIsLoaded] = useState(false);
     const [isUpdated, setIsUpdated] = useState(true);
-    const [currentValuesUpdated, setCurrentValuesUpdated] = useState(true);
-    const [currentCategory, setCurrentCategory] = useState([]);
-    const [currentDuration, setCurrentDuration] = useState([]);
-    const [currentpassExpireInDay, setCurrentPassExpireInDay] = useState([]);
-    const [currentPrice, setCurrentPrice] = useState([]);
+    const [currentTitleUpdated, setCurrentTitleUpdated] = useState(true);
+    
     const { data } = useMultiFetch();
-    const categoryDropdown = useRef();
-    const categoryTypeField = useRef();
-    const durationField = useRef();
-    const expiretyField = useRef();
-    const priceField = useRef();
+    const titleDropdown = useRef();
+    const titleTypeField = useRef();
+    const descriptionField = useRef();
+    const articleTextField = useRef();
+    let imgField = '';
 
     const isDataLoaded = () => {
       return listOfNews.length > 0;
@@ -30,81 +27,97 @@ export const NewsModify = () => {
       if(role() !== "EMPLOYEE"){
         navigate("/map");
       }
-      const categoryUrl = '/category/all';
+      const newsUrl = '/news/api/all';
       if (isUpdated)
-        (async () => setListOfNews(await data(categoryUrl), setIsUpdated(false)))();
+        (async () => setListOfNews(await data(newsUrl), setCurrentTitleUpdated(await data(newsUrl)),setIsUpdated(false)))();
       if (isDataLoaded() && !isLoaded){
-        setCurrentCategory(listOfNews[0].category);
-        setCurrentDuration(listOfNews[0].passDuration);
-        setCurrentPassExpireInDay(listOfNews[0].passExpireInDay);
-        setCurrentPrice(listOfNews[0].price);  
         setIsLoaded(true);
       }
-    }, [isUpdated,currentValuesUpdated])
+    }, [isUpdated,currentTitleUpdated])
   
-    const getModifiedCategory = () => listOfNews.filter((category) => category.id == categoryDropdown.current.value);
+    const getModifiedNews = () => listOfNews.filter((news) => news.title === titleDropdown.current.value);
   
     const changeCategory = () => {
-      const categoryId = getModifiedCategory();
-      setCurrentValuesUpdated(!currentValuesUpdated);
-      setCurrentCategory(categoryId[0].category);      
-      setCurrentDuration(categoryId[0].passDuration);
-      setCurrentPassExpireInDay(categoryId[0].passExpireInDay);
-      setCurrentPrice(categoryId[0].price);
-    
+      const newsId = getModifiedNews();
+      setCurrentTitleUpdated(newsId);
+      setIsLoaded(false);
     }
+
+  async function getByteArray(event) {
+      let myFile = event.target.files[0];
+      let byteArray = await fileToByteArray(myFile);
+      
+      console.log(byteArray);
+      imgField = byteArray;
+  }
+  
+  function fileToByteArray(file) {
+    return new Promise((resolve, reject) => {
+        try {
+            let reader = new FileReader();
+            let fileByteArray = [];
+            reader.readAsArrayBuffer(file);
+            reader.onloadend = (evt) => {
+                if (evt.target.readyState == FileReader.DONE) {
+                    let arrayBuffer = evt.target.result,
+                    array = new Uint8Array(arrayBuffer);
+                    for (let i = 0; i < array.length; i++) {
+                        fileByteArray.push(array[i]);
+                    }
+                }
+                resolve(fileByteArray);
+            }
+        }
+        catch (e) {
+            reject(e);
+        } 
+    })
+}
   
     const updateRoute = async () => {
-      const categoryId = getModifiedCategory("id").id;
-      const categoryName = categoryTypeField.current.value;
-      const durationOfCategory = durationField.current.value;
-      const expirationOfCategory = expiretyField.current.value;
-      const priceOfCategory = priceField.current.value;
+      const newsTitle = titleTypeField.current.value;
+      const newsDescription = descriptionField.current.value;
+      const newsArticleText = articleTextField.current.value;
+      const newsImgData = imgField.current.value;
   
-      const categoryUpdateURL = '/category/api';
-      const categoryObject = {
-        id: categoryId,
-        category: categoryName,
-        passDuration: durationOfCategory,
-        passExpireInDay: expirationOfCategory,
-        price: priceOfCategory
+      const newsUpdateURL = '/news/api';
+      const newsObject = {
+        title: newsTitle,
+        description: newsDescription,
+        articleText: newsArticleText,
+        imgField: newsImgData
       }
-      await data(categoryUpdateURL, 'PUT', categoryObject);
+      await data(newsUpdateURL, 'PUT', newsObject);
       navigate('/workspace');
     }
     if (isDataLoaded() && isLoaded) {
     return (
           <div className='pageContent'>
-            <h2>Modify transportation route</h2>
+            <h2>Modify news article</h2>
             <div className='pagePanel'>
               <div className='pageElement'>
                 <div className='routeDetail'>
-                  <p>Select existing category:</p>
-                  <select ref={categoryDropdown} onChange={() => changeCategory()}>
-                    {listOfNews.map((category) => <option key={category.id}>{category.id}</option>)}
+                  <p>Select existing article:</p>
+                  <select ref={titleDropdown} defaultValue={currentTitleUpdated[0].title} onChange={() => changeCategory()}>
+                    {listOfNews.map((news) => <option key={news.id}>{news.title}</option>)}
                   </select>
-                  <p>Rename category type</p>
+                  <p>Rename article title</p>
                   <input 
-                    ref={categoryTypeField}
-                    defaultValue={currentCategory}
+                    ref={titleTypeField}
+                    defaultValue={currentTitleUpdated[0].title}
                   />
-                  <p>Rename category duration:</p>
-                  <input 
-                    ref={durationField}
-                    defaultValue={currentDuration}
+                  <p>Change article description:</p>
+                  <textarea
+                    ref={descriptionField}
+                    defaultValue={currentTitleUpdated[0].description}
                   />
-                  <p>Change expirety to:</p>
-                  <input 
-                    type="number" 
-                    ref={expiretyField}
-                    defaultValue={currentpassExpireInDay}  
+                  <p>Change article text:</p>
+                  <textarea 
+                    ref={articleTextField}
+                    defaultValue={currentTitleUpdated[0].articleText}  
                   />
-                  <p>Change price to:</p>
-                  <input 
-                    type="number" 
-                    ref={priceField}
-                    defaultValue={currentPrice}  
-                  />
+                  <p>Change image to:</p>
+                  <input type="file" onChange={(event) => getByteArray(event)}/>
                 </div>
                 <button onClick={() => updateRoute()}>Update</button>
               </div>
