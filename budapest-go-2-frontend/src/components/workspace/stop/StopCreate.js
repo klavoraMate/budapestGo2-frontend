5337c26d36acbf365398f35a4f6471a9c24939e4
Kieferer <balazsborsi0@gmail.com>
2023-05-26 03:46:53 +0200

    feat: Add updateInputLatAndLng, updateMapLatAndLng, updateMarker, MapEditor to StopCreate

--- budapest-go-2-frontend/src/components/workspace/stop/StopCreate.js
+++ budapest-go-2-frontend/src/components/workspace/stop/StopCreate.js
@@ -1,12 +1,16 @@
-import { useEffect, useRef } from 'react'
+import {useEffect, useRef, useState} from 'react'
 import { useNavigate } from "react-router-dom";
 import useMultiFetch from '../../api/useMultiFetch';
 import { role } from '../../token/TokenDecoder';
+import MapEditor from "../../elements/mapEditor/MapEditor";
+import './stopMap.css'
 
 function StopCreate() {
   const stopName = useRef();
   const stopLatitude = useRef();
   const stopLongitude = useRef();
+  const markerRef = useRef([0, 0]);
+  const locationOfBudapest = [47.486208, 19.108459];
   const navigate = useNavigate();
   const { data } = useMultiFetch();
 
@@ -27,23 +31,52 @@ function StopCreate() {
     }
   }, [])
 
+  function updateInputLatAndLng() {
+    stopLatitude.current.value = (markerRef.current?.value.lat);
+    stopLongitude.current.value = (markerRef.current?.value.lng);
+  }
+
+  const updateMapLatAndLng = () => {
+    markerRef.current.value.lat = stopLatitude.current?.value;
+    markerRef.current.value.lng = stopLongitude.current?.value;
+    updateMarker();
+  }
+
+  const [markerKey, setMarkerKey] = useState(0);
+  const updateMarker = () => {
+    setMarkerKey(prevKey => prevKey + 1);
+  }
+
   return (
-    <div className='pageContent'>
-      <h2>Create transportation stop</h2>
-      <div className='pagePanel'>
-        <div className='pageElement'>
+    <>
+    <div className='as'>
           <div className='routeDetail'>
+      <h2>Create transportation stop</h2>
             <p>Set name of new stop:</p>
             <input ref={stopName}/>
             <p>Set latitude:</p>
-            <input type="number" step="0.000001" ref={stopLatitude}/>
+            <input type="number" step="0.000001"
+                   ref={stopLatitude}
+                   onChange={() => updateMapLatAndLng()}
+                   defaultValue={locationOfBudapest[0]}
+            />
             <p>Set longitude:</p>
-            <input type="number" step="0.000001" ref={stopLongitude}/>
+            <input type="number" step="0.000001"
+                   ref={stopLongitude}
+                   onChange={() => updateMapLatAndLng()}
+                   defaultValue={locationOfBudapest[1]}
+            />
           </div>
-          <button onClick={() => createStop()}>Create</button>
-        </div>
-      </div>
+          <button className={"stopButton"} onClick={() => createStop()}>Create</button>
     </div>
+      <div className={"pageElement"}>
+        <MapEditor
+          onClick={() => updateInputLatAndLng()}
+          ref={markerRef} vehicleCategory={"default"}
+          markerKey={markerKey}
+        />
+      </div>
+    </>
   )
 }
 
